a0702202cbcc11ea906f8a169fc874fb71080ee0
ImMALWARE <notmalware@inbox.ru>
2024-12-23 13:44:46 +0300

    Кнопка Action для остановки/перезапуска, поддержка большего количества процессоров (спасибо @MrCoder228re), кнопка Update, новые конфиги

---
+++ action.sh
@@ -0,0 +1,21 @@
+#!/system/bin/env sh
+
+arch=$(uname -m)
+
+case $arch in
+    x86_64)            nfqws=nfqws_x86_64 ;;
+    i386|i686)         nfqws=nfqws_x86 ;;
+    armv7l|arm)        nfqws=nfqws_arm32 ;;
+    aarch64)           nfqws=nfqws_arm64 ;;
+    mips)              nfqws=nfqws_mips ;;
+    mipsel)            nfqws=nfqws_mipsel ;;
+    *)                 echo "Unknown arch: $arch"; exit 1 ;;
+esac
+
+if pidof "$nfqws" > /dev/null; then
+    echo "Stopping..."
+    zapret stop
+else
+    echo "Running..."
+    zapret start
+fi
