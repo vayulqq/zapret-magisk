a1a8a9f9db2b62fb028d8164752cb65d98e9b502
ImMALWARE <notmalware@inbox.ru>
2025-01-08 22:56:24 +0300

    Fix for armv8l CPUs

--- action.sh
+++ action.sh
@@ -2,14 +2,29 @@
 
 arch=$(uname -m)
 
-case $arch in
-    x86_64)            nfqws=nfqws_x86_64 ;;
-    i386|i686)         nfqws=nfqws_x86 ;;
-    armv7l|arm)        nfqws=nfqws_arm32 ;;
-    aarch64)           nfqws=nfqws_arm64 ;;
-    mips)              nfqws=nfqws_mips ;;
-    mipsel)            nfqws=nfqws_mipsel ;;
-    *)                 echo "Unknown arch: $arch"; exit 1 ;;
+case "$arch" in
+    "x86_64")
+        nfqws="nfqws_x86_64"
+        ;;
+    "i386"|"i686")
+        nfqws="nfqws_x86"
+        ;;
+    "armv7l"|"arm"|"armv8l")
+        nfqws="nfqws_arm32"
+        ;;
+    "aarch64")
+        nfqws="nfqws_arm64"
+        ;;
+    "mips")
+        nfqws="nfqws_mips"
+        ;;
+    "mipsel")
+        nfqws="nfqws_mipsel"
+        ;;
+    *)
+        echo "Unknown arch: $arch"
+        exit 1
+        ;;
 esac
 
 if pidof "$nfqws" > /dev/null; then
