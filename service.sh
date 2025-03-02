704cb2aabec6f0f55a54ac894c22ed9c64ce2f4b
NTeditor <nteditor4@gmail.com>
2025-03-02 21:37:10 +0300

    Добавлена команда для включения/выключения автозапуска.

--- service.sh
+++ service.sh
@@ -1,5 +1,10 @@
 #!/system/bin/sh
+AUTOSTART="true"
 
-if [ -f /data/adb/modules/zapret/autostart ]; then
+# if [ -f /data/adb/modules/zapret/autostart ]; then
+#     su -c "zapret start"
+# fi
+
+if [ "AUTOSTART" == "true" ]; then
     su -c "zapret start"
-fi
+fi;
